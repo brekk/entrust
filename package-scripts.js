ae12ead86e64c3b2406fd80b38fc5375401940ce
brekk <brekk@brekkbockrath.com>
2017-09-18 12:10:39 -0700

    fixes

--- package-scripts.js
+++ package-scripts.js
@@ -1,4 +1,6 @@
 const germs = require(`germs`)
 const pkg = require(`./package.json`)
 
-module.exports = germs(pkg.name)
+module.exports = germs(pkg.name, {
+  readme: `documentation readme README.md -s "API" src/*.js`
+})
