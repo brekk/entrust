639aece3b21da56bb27fad015da007c0ea163868
Brekk <brekk@brekkbockrath.com>
2017-09-18 19:18:52 -0700

    F/germ (#12)

--- package-scripts.js
+++ package-scripts.js
@@ -1,28 +1,6 @@
-module.exports = {
-  scripts: {
-    dependencies: `depcheck`,
-    graph: [
-      `madge src --json | jayin "_.toPairs(x).map(([k, v]) =>`,
-      `([k,_.map(v, (y) => y.indexOf('node_modules') > -1 ?`,
-      `y.substr(y.indexOf('node_modules') + 13) : y)]))`,
-      `.filter(([k, v]) => !(k.indexOf('spec') > -1))`,
-      `.filter(([k, v]) => !(k.indexOf('scss') > -1))`,
-      `.filter(([k, v]) => !(k.indexOf('css') > -1))`,
-      `.reduce((agg, [k, v]) => Object.assign({}, agg, {[k]: v}), {})"`,
-      ` | madge --stdin --image dependencies.svg`
-    ].join(``),
-    documentation: `documentation build src/** -f html -o docs`,
-    readme: `documentation readme README.md -s "API" src/index.js`,
-    lint: {
-      default: `nps lint.src && nps lint.jsdoc`,
-      src: `eslint src/*.js`,
-      jsdoc: `documentation lint src/index.js`
-    },
-    build: {
-      default: `nps build.main`,
-      main: `rollup -c config/rollup.config.main.js`
-    },
-    dueDiligence: `nps lint && nps build && nps readme`,
-    test: `echo 'trust!'`
-  }
-}
+const germs = require(`germs`)
+const pkg = require(`./package.json`)
+
+module.exports = germs(pkg.name, {
+  readme: `documentation readme README.md -s "API" src/*.js`
+})
---
+++ src/index.spec.js
@@ -0,0 +1,131 @@
+/* global test */
+import {t} from './test-helpers'
+import {
+  eN, eD,
+  e0,
+  e1,
+  e2,
+  e3,
+  e4,
+  e5,
+  e6,
+  e7,
+  e8,
+  e9,
+  e10
+} from './index'
+
+test(`e0`, () => {
+  t.is(typeof e0, `function`)
+  t.is(e0(`prop`, {prop: () => 100}), 100)
+})
+
+test(`e1`, () => {
+  t.is(typeof e1, `function`)
+  t.is(e1(`prop`, 10, {prop: (x) => x * 2}), 20)
+})
+
+test(`e1 -- map example`, () => {
+  t.is(typeof e1, `function`)
+  t.deepEqual(e1(`map`, (x) => x * 2, [1, 2, 3, 4]), [2, 4, 6, 8])
+})
+
+test(`e2`, () => {
+  t.is(typeof e2, `function`)
+  t.is(e2(`prop`, 10, 11, {prop: (x, y) => x * y}), 110)
+})
+test(`e2 -- reduce example`, () => {
+  t.is(typeof e2, `function`)
+  t.is(e2(`reduce`, (a, b) => a + b, 0, [1, 2, 3, 4]), 10)
+})
+test(`e3`, () => {
+  t.is(typeof e3, `function`)
+  t.is(e3(`method`, 1, 2, 3, {method: (a, b, c) => a + b + c}), 6)
+})
+test(`e4`, () => {
+  t.is(typeof e4, `function`)
+  t.is(e4(`method`, 1, 2, 3, 4, {method: (a, b, c, d) => a + b + c + d}), 10)
+})
+test(`e5`, () => {
+  t.is(typeof e5, `function`)
+  t.is(e5(`method`, 1, 2, 3, 4, 5, {method: (a, b, c, d, e) => a + b + c + d + e}), 15)
+})
+test(`e6`, () => {
+  t.is(typeof e6, `function`)
+  t.is(
+    e6(`method`,
+      1, 2, 3, 4, 5, 6,
+      { method:
+        (a, b, c, d, e, f) => a + b + c + d + e + f
+      }
+    ),
+    21
+  )
+})
+test(`e7`, () => {
+  t.is(typeof e7, `function`)
+  t.is(
+    e7(`method`,
+      1, 2, 3, 4, 5, 6, 7,
+      { method:
+        (a, b, c, d, e, f, g) => a + b + c + d + e + f + g
+      }
+    ),
+    28
+  )
+})
+test(`e8`, () => {
+  t.is(typeof e8, `function`)
+  t.is(
+    e8(`method`,
+      1, 2, 3, 4, 5, 6, 7, 8,
+      { method:
+        (a, b, c, d, e, f, g, h) => a + b + c + d + e + f + g + h
+      }
+    ),
+    36
+  )
+})
+test(`e9`, () => {
+  t.is(typeof e9, `function`)
+  t.is(
+    e9(`method`,
+      1, 2, 3, 4, 5, 6, 7, 8, 9,
+      { method:
+        (a, b, c, d, e, f, g, h, i) => a + b + c + d + e + f + g + h + i
+      }
+    ),
+    45
+  )
+})
+test(`e10`, () => {
+  t.is(typeof e10, `function`)
+  t.is(
+    e10(`method`,
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+      { method:
+        (a, b, c, d, e, f, g, h, i, j) => a + b + c + d + e + f + g + h + i + j
+      }
+    ),
+    55
+  )
+})
+test(`eN`, () => {
+  t.is(typeof eN, `function`)
+  t.is(
+    eN(3, `method`, [1, 2, 3], {method: (a, b, c) => a + b + c}),
+    6
+  )
+})
+test(`eD`, () => {
+  t.is(typeof eD, `function`)
+  t.is(
+    eD(3, `method`, [1, 2, 3], {method: (a, b, c) => a + b + c}),
+    6
+  )
+  t.throws(
+    () => eD(3, `method`, [1, 2], {method: (a, b, c) => a + b + c}),
+    // n,m,a,d
+    `method expects total args (2) to equal the given arity (3)`
+  )
+})
