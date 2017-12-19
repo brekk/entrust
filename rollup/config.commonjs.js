eba0c4b9be35ba03c84d2b766ba14f818ae81710
Brekk <brekk@brekkbockrath.com>
2017-12-19 07:06:22 -0800

    updates (#51)

--- rollup/config.commonjs.js
+++ rollup/config.commonjs.js
@@ -1,18 +1,39 @@
 const pkg = require(`../package.json`)
+const commonjs = require(`rollup-plugin-commonjs`)
 const {bundle} = require(`germs`)
-
-const external = (
-  pkg && pkg.dependencies ?
-    Object.keys(pkg.dependencies) :
-    []
-)
 
 module.exports = bundle({
   name: pkg.name,
-  external,
+  alias: {
+  },
   input: `src/index.js`,
   output: {
+    name: `FUTILITY`,
     file: `./${pkg.name}.js`,
-    format: `cjs`
+    format: `umd`
+  },
+  alterPlugins: (plug) => {
+    plug[3] = commonjs({
+      extensions: [`.js`],
+      include: `node_modules/**`,
+      namedExports: {
+        'node_modules/katsu-curry/katsu-curry.js': [
+          `pipe`,
+          `compose`,
+          `$`,
+          `PLACEHOLDER`,
+          `curryify`,
+          `curry`,
+          `curryObjectK`,
+          `curryObjectN`,
+          `curryObjectKN`,
+          `remap`,
+          `remapArray`,
+          `K`,
+          `I`
+        ]
+      }
+    })
+    return plug
   }
 })
--- wallaby.js
+++ wallaby.js
@@ -1,36 +1,62 @@
-const pkg = require('./package.json')
+const pkg = require(`./package.json`)
 
-module.exports = function configureWallaby(wallaby) {
+module.exports = function configureWallaby() {
   return {
-    name: `entrust`,
+    name: pkg.name,
+    debug: true,
     files: [
-      `src/**/*.js`,
-      `src/*.js`
+      `src/*.js`,
+      `!src/*.spec.js`
     ],
 
     tests: [
-      `tests/**/*.spec.js`,
-      `tests/*.spec.js`
+      `src/*.spec.js`,
+      // wallaby doesn't know about this yet
+      `!src/bundle.spec.js`
     ],
 
     env: {
       type: `node`,
-      kind: `electron`
+      runner: `node`
     },
 
-    compilers: {
-      '**/*.js': wallaby.compilers.babel()
+    // compilers: {
+    //   '**/*.js': wallaby.compilers.babel()
+    // },
+    preprocessors: {
+      '**/*.js': (file) => require(`babel-core`).transform(
+        file.content, {
+          sourceMap: true,
+          presets: [`es2015`],
+          plugins: [`transform-object-rest-spread`]
+        }
+      )
     },
 
     testFramework: `jest`,
 
-    setup: function setup() {
+    setup: function setup(w) {
       require(`babel-polyfill`)
+      w.testFramework.configure({
+        "modulePaths": [
+          `src`
+        ],
+        "moduleDirectories": [
+          `node_modules`,
+          `src`
+        ],
+        "mapCoverage": true,
+        "moduleFileExtensions": [
+          `js`,
+          `json`
+        ],
+        "testMatch": [
+          `**/*.spec.(jsx|js)`
+        ]
+      })
     },
-
-    debug: true,
     filesWithNoCoverageCalculated: [
-
+      // `src/core/fs.js`
     ]
   }
 }
