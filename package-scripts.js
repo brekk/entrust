d2f6b4cb846c9e38e46d4e09fb0b97882c0d9f48
brekk <brekk@brekkbockrath.com>
2018-02-16 01:42:02 -0800

    I AM CLINTON. AND I AM DOLE. IT DOES NOT MATTER WHICH ONE OF US YOU CHOOSE SO LONG AS YOU CHOOSE ONE OF US

--- package-scripts.js
+++ package-scripts.js
@@ -1,9 +1,20 @@
 const germs = require(`germs`)
 const pkg = require(`./package.json`)
-// const utils = require(`nps-utils`)
+const utils = require(`nps-utils`)
 
-// const allNPS = utils.concurrent.nps
-module.exports = germs.build(pkg.name, {
+const allNPS = utils.concurrent.nps
+const GERMS = germs.build(pkg.name, {
   readme: `documentation readme -s "API" src/*.js`,
-  test: `jest src/*.spec.js --coverage --coveragePathIgnorePatterns entrust.js common-tags katsu-curry`
+  test: [
+    `jest src/*.spec.js --coverage`,
+    `--coveragePathIgnorePatterns entrust.js common-tags katsu-curry`
+  ].join(` `)
 })
+
+GERMS.scripts.lint.project = `clinton`
+GERMS.scripts.lint = Object.assign(
+  {},
+  GERMS.scripts.lint,
+  {script: allNPS(`lint.src`, `lint.jsdoc`, `lint.project`)}
+)
+module.exports = GERMS
